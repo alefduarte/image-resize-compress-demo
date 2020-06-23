017611ebbe3ad51b3d5c3103bda5d83710a123bc
Alef Duarte <alef.duarte@outlook.com>
2020-06-23 14:10:06 -0300

    Updates

---
+++ precache-manifest.399e23dabb56c97659e05e7148a4c086.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeb0c03c7dc0c3b7a9daef5a6d092dfe",
+    "url": "/image-resize-compress-demo/index.html"
+  },
+  {
+    "revision": "8e40fd215d8152f0d068",
+    "url": "/image-resize-compress-demo/static/css/main.3a1e0923.chunk.css"
+  },
+  {
+    "revision": "b2d74c69c4cf126b26a9",
+    "url": "/image-resize-compress-demo/static/js/2.a9ed2d57.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/image-resize-compress-demo/static/js/2.a9ed2d57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e40fd215d8152f0d068",
+    "url": "/image-resize-compress-demo/static/js/main.e1fbdab7.chunk.js"
+  },
+  {
+    "revision": "651c8393aa5cd812269a",
+    "url": "/image-resize-compress-demo/static/js/runtime-main.f6345295.js"
+  }
+]);
